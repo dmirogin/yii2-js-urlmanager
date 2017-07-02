73c3b64ad6c1973471f5910853db2e165c7352f1
Dmitry Dorogin <dmirogin@ya.ru>
2017-07-02 19:14:52 +0300

    fix collection rules if enablePrettyUrl is false

--- src/JsUrlManager.php
+++ src/JsUrlManager.php
@@ -8,6 +8,7 @@ use yii\base\Object;
 use yii\helpers\Json;
 use yii\base\Application;
 use yii\web\JsExpression;
+use yii\web\UrlRule;
 use yii\web\View;
 
 /**
@@ -42,11 +43,19 @@ class JsUrlManager extends Object implements BootstrapInterface
     public function defineConfiguration(Application $app)
     {
         $rules = [];
-        foreach ($app->urlManager->rules as $rule) {
-            $rules[] = [
-                'name' => $rule->name,
-                'route' => $rule->route
-            ];
+        foreach ($app->urlManager->rules as $name => $rule) {
+            if ($rule instanceof UrlRule) {
+                $rules[] = [
+                    'name' => $rule->name,
+                    'route' => $rule->route
+                ];
+            } else if (is_string($rule)) {
+                $rules[] = [
+                    'name' => $name,
+                    'route' => $rule
+                ];
+            }
+
         }
 
         return [
